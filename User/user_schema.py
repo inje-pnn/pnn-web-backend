86a8b88e01c51710b55c5ee59f13f6b96da5dd39
Wooju8303 <wooju8303@gmail.com>
2025-01-08 13:04:56 +0900

    #25.01.08 feat: Google OAuth
    - Google OAuth를 이용한 소셜 로그인 기능 작업 중
    - 사용자 인증 및 회원가입 처리 로직 초안 작성
    - JWT 토큰 발급 로직 일부 구현
    - 에러 처리 및 테스트 코드 미완료

--- User/user_schema.py
+++ User/user_schema.py
@@ -1,5 +1,23 @@
-from pydantic import BaseModel
+###
+from typing import Optional
+from pydantic import BaseModel, EmailStr
 
-class ExampleDTO(BaseModel):
-    name: str
-    age: int
+class authRequest(BaseModel):
+    code:str
+
+class SocialMember(BaseModel):
+    name :str
+    email: str
+
+class UserCreateRequest(BaseModel):
+    email: EmailStr  # 이메일 형식 자동 검증
+    student_id: str  # 학번
+    year: int        # 학년
+    name: str        # 이름
+    github_url: Optional[str] = None  # 선택적 필드
+
+class AddInfoRequest(BaseModel):
+    email: EmailStr
+    student_id: str
+    year: int
+    github_url: Optional[str] = None
