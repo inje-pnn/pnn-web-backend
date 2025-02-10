9543618a17d05407d92daab596c4649fa1b51b6a
hopar <mkiu7030@naver.com>
2025-02-10 18:34:42 +0900

    [PIX] project dto 오타 수정

--- project/project_router.py
+++ project/project_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import provide_session
 from project.project_crud import ProjectCrud
-from project.project_schema import ProjcetDTO, TypeDTO
+from project.project_schema import ProjectDTO, TypeDTO
 
 project_router = APIRouter(
     prefix="/project",
@@ -11,7 +11,7 @@ project_router = APIRouter(
 )
 
 @project_router.post("/create")
-async def create_project(payload: ProjcetDTO, session:AsyncSession=Depends(provide_session)):
+async def create_project(payload: ProjectDTO, session:AsyncSession=Depends(provide_session)):
     
     crud=ProjectCrud(session)
     return await crud.create(payload)
